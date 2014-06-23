f61cdcd35316a80f6748ebcdc625ca3e725cd309
Shanyue HUANG <yocliantion@gmail.com>
2014-06-23 14:23:19 +0100

    v2

---
+++ v1/Assets/CubeFurnishing.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+var myCubeFurniture: GameObject;
+
+function Start () {
+
+}
+
+function Update () {
+	//Calculation
+
+	//Generate
+	if(Input.GetKey(KeyCode.Space)){
+	var myCube:GameObject = Instantiate(myCubeFurniture, transform.position+Vector3(0,100,0), transform.rotation);
+	
+	
+	Debug.Log("Furnished!",myCube);
+	}
+	
+
+}
