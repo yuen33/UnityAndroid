a28aef452a6cf4766864432b747e5b24c5aa7993
Shanyue HUANG <yocliantion@gmail.com>
2014-06-27 14:26:58 +0100

    massive, give up version, plan to roll back

--- v3/Assets/Autofurnishing/scripts/Populating.cs
+++ v3/Assets/Autofurnishing/scripts/Populating.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 
 public class Populating : MonoBehaviour {
-	public GameObject dummy;
-	public GameObject mycube;
+//	public GameObject dummy;
+//	public GameObject mycube;
 //	public GameObject room;
 	//TODO: get the unknown prefab extents
-	public Vector3 mycubeExtents;
+//	public Vector3 mycubeExtents;
 
 	public Vector3 offset;
 	public Vector3 roomcenter;
@@ -16,11 +16,16 @@ public class Populating : MonoBehaviour {
 	private GameObject currentObject;
 	private bool isSearching=false;
 
+	public static int MAX_NumOfT1=20;
+	public static int MAX_InfoItems=2;
+	public bool[] Tier1Check= new bool[MAX_NumOfT1];
+	public Vector3[,] Tier1Data= new Vector3[MAX_NumOfT1,MAX_InfoItems];
+
 	// Use this for initialization
 	Vector3 getRandomPosition(){
 		Vector3 randomPosition;
 		randomPosition.x=(roomcenter.x-offset.x)+Random.value * 2* offset.x;
-		randomPosition.y= mycubeExtents.y;//*********TODO !!!
+		randomPosition.y= PrefabsManager.Instance.BigPiece.transform.lossyScale.y;//*********TODO !!!
 		//To make it over the floor, extents=000 for any prefabs not in the scene
 		randomPosition.z=(roomcenter.z-offset.z)+Random.value * 2* offset.z;
 
@@ -31,15 +36,15 @@ public class Populating : MonoBehaviour {
 		counter++;
 		Debug.Log("counter="+counter);
 
-		GameObject clone=(GameObject)Instantiate(mycube, transform.position+randomPosition, transform.rotation);//TODO rotation etc.
+		GameObject clone=(GameObject)Instantiate(PrefabsManager.Instance.BigPiece, transform.position+randomPosition, transform.rotation);//TODO rotation etc.
 		clone.gameObject.layer=9;//the Layer 8 is the floorplan meshes
 		
 		//Write into the inRoomRetrival class
-		InRoomRetrival.Instance.Tier1Check[counter]=true;//seems useless..in this class, as I have set a counter; ?_?
-		InRoomRetrival.Instance.Tier1Data[counter,0]=clone.collider.bounds.center;
+		Tier1Check[counter]=true;//seems useless..in this class, as I have set a counter; ?_?
+		Tier1Data[counter,0]=clone.collider.bounds.center;
 //		Debug.Log("clone.collider.bounds.center"+clone.collider.bounds.center);
-		Debug.Log("Write in: InRoomRetrival.Instance.Tier1Data["+counter+",0]="+InRoomRetrival.Instance.Tier1Data[counter,0]);
-		InRoomRetrival.Instance.Tier1Data[counter,1]=clone.collider.bounds.extents;
+		Debug.Log("Write in: Tier1Data["+counter+",0]="+Tier1Data[counter,0]);
+		Tier1Data[counter,1]=clone.collider.bounds.extents;
 //		Debug.Log("clone.collider.bounds.extents"+clone.collider.bounds.extents);
 //		Debug.Log("InRoomRetrival.Instance.Tier1Data[counter,1]"+InRoomRetrival.Instance.Tier1Data[counter,1]);
 
@@ -47,39 +52,50 @@ public class Populating : MonoBehaviour {
 	}
 
 	void Start () {
-		mycubeExtents=0.5f*mycube.transform.lossyScale;
-		offset=gameObject.collider.bounds.extents-mycubeExtents;//roomsize = 2* offset //*********TODO !!!
+		for(int i=0; i<= MAX_NumOfT1;i++){
+			Tier1Check[i]=false;
+			for(int j=0; j<MAX_InfoItems;j++){
+				Tier1Data[i,j]=new Vector3(0f,0f,0f);
+			}
+		}
+
+//		dummy=PrefabsManager.Instance.Dummy;
+//		mycube=PrefabsManager.Instance.BigPiece;
+//
+//		mycubeExtents=0.5f*mycube.transform.lossyScale;
+
+		offset=gameObject.collider.bounds.extents-PrefabsManager.Instance.BigPiece.transform.lossyScale;//roomsize = 2* offset //*********TODO !!!
 //		Debug.Log("room.collider.bounds.extents"+room.collider.bounds.extents);
 		roomcenter=gameObject.collider.bounds.center;
 //		Debug.Log("room.collider.bounds.center"+room.collider.bounds.center);
 
-		Vector3 randomPosition=getRandomPosition();
-
-//		currentObject=Clone(randomPosition);
-		//-----------------------------------Changed for the first cube bug-------------------------------
-		counter++;
-		Debug.Log("counter="+counter);
-		
-		currentObject=(GameObject)Instantiate(dummy, transform.position+randomPosition, transform.rotation);//TODO rotation etc.
-		currentObject.gameObject.layer=9;//the Layer 8 is the floorplan meshes
-
-		/**
-		 * If I didn't write in, the second cube will have the "first cube problem" :(
-		 */
-		//Write into the inRoomRetrival class
-		InRoomRetrival.Instance.Tier1Check[counter]=true;//seems useless..in this class, as I have set a counter; ?_?
-		InRoomRetrival.Instance.Tier1Data[counter,0]=currentObject.collider.bounds.center;
-		//		Debug.Log("clone.collider.bounds.center"+clone.collider.bounds.center);
-		Debug.Log("Write in: InRoomRetrival.Instance.Tier1Data["+counter+",0]="+InRoomRetrival.Instance.Tier1Data[counter,0]);
-		InRoomRetrival.Instance.Tier1Data[counter,1]=currentObject.collider.bounds.extents;
-
-		/** Solution 1: To destroy it later
-		currentObject.name="dummy";
-		*/
-
-		//Solution 2 change render.enable to false
-		currentObject.renderer.enabled=false;
-		//-------------------------------------------------------------------------------------------------
+//		Vector3 randomPosition=getRandomPosition();
+//
+////		currentObject=Clone(randomPosition);
+//		//-----------------------------------Changed for the first cube bug-------------------------------
+//		counter++;
+//		Debug.Log("counter="+counter);
+//		
+//		currentObject=(GameObject)Instantiate(dummy, transform.position+randomPosition, transform.rotation);//TODO rotation etc.
+//		currentObject.gameObject.layer=9;//the Layer 8 is the floorplan meshes
+//
+//		/**
+//		 * If I didn't write in, the second cube will have the "first cube problem" :(
+//		 */
+//		//Write into the inRoomRetrival class
+//		InRoomRetrival.Instance.Tier1Check[counter]=true;//seems useless..in this class, as I have set a counter; ?_?
+//		InRoomRetrival.Instance.Tier1Data[counter,0]=currentObject.collider.bounds.center;
+//		//		Debug.Log("clone.collider.bounds.center"+clone.collider.bounds.center);
+//		Debug.Log("Write in: InRoomRetrival.Instance.Tier1Data["+counter+",0]="+InRoomRetrival.Instance.Tier1Data[counter,0]);
+//		InRoomRetrival.Instance.Tier1Data[counter,1]=currentObject.collider.bounds.extents;
+//
+//		/** Solution 1: To destroy it later
+//		currentObject.name="dummy";
+//		*/
+//
+//		//Solution 2 change render.enable to false
+//		currentObject.renderer.enabled=false;
+//		//-------------------------------------------------------------------------------------------------
 
 	}
 
@@ -87,12 +103,12 @@ public class Populating : MonoBehaviour {
 		Debug.Log("*********************************is Searching...***************************************");
 
 		//whether it is in allocated arena
-		for(int i=1;i<=counter;i++){
-			Vector3 CentresDistance= InRoomRetrival.Instance.Tier1Data[i,0]-randomPosition;
-			Vector3 fetchedExtents= InRoomRetrival.Instance.Tier1Data[i,1];
+		for(int i=0;i<=counter;i++){
+			Vector3 CentresDistance= Tier1Data[i,0]-randomPosition;
+			Vector3 fetchedExtents= Tier1Data[i,1];
 
 //			Debug.Log("Read in: InRoomRetrival.Instance.Tier1Check[counter]="+InRoomRetrival.Instance.Tier1Check[i]);
-			Debug.Log("Read in: InRoomRetrival.Instance.Tier1Data["+i+",0]="+InRoomRetrival.Instance.Tier1Data[i,0]);
+			Debug.Log("Read in: InRoomRetrival.Instance.Tier1Data["+i+",0]="+Tier1Data[i,0]);
 //			Debug.Log("randomPosition"+randomPosition);
 
 //			Debug.Log("CentresDistance.x"+CentresDistance.x);
@@ -101,8 +117,8 @@ public class Populating : MonoBehaviour {
 //			Debug.Log("CentresDistance.z"+CentresDistance.z);
 //			Debug.Log("fetchedExtents.z+mycubeExtents.z"+fetchedExtents.z+mycubeExtents.z);
 
-			if( Mathf.Abs(CentresDistance.x) > fetchedExtents.x+mycubeExtents.x ||
-			    Mathf.Abs(CentresDistance.z) > fetchedExtents.z+mycubeExtents.z){
+			if( Mathf.Abs(CentresDistance.x) > fetchedExtents.x+PrefabsManager.Instance.BigPiece.transform.lossyScale.x ||
+			   Mathf.Abs(CentresDistance.z) > fetchedExtents.z+PrefabsManager.Instance.BigPiece.transform.lossyScale.z){
 				Debug.Log("It is not overlapped with Cube"+i);
 				continue;
 			}else{
@@ -131,8 +147,8 @@ public class Populating : MonoBehaviour {
 		if(Input.GetKeyDown(KeyCode.K)){
 			isSearching=false;
 			//Destroy(GameObject.Find("dummy"));
-			for(int i=1;i<=counter;i++){
-				Debug.Log("Cube"+i+" centre: "+InRoomRetrival.Instance.Tier1Data[i,0]);
+			for(int i=0;i<=counter;i++){
+				Debug.Log("Cube"+i+" centre: "+Tier1Data[i,0]);
 			}
 		}
 
---
+++ v3/Assets/Autofurnishing/scripts/PrefabsManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrefabsManager : MonoBehaviour {
+	private static PrefabsManager instance = null;
+
+	// to create a singleton which holds the variables
+	public static PrefabsManager Instance{
+		get{
+			if(instance==null){
+				instance=new GameObject("PrefabsManager").AddComponent<PrefabsManager>();
+			}
+			return instance;
+		}
+	}
+	public GameObject Dummy;
+	public GameObject BigPiece;
+	public GameObject Seat;
+	public GameObject SmallPiece;
+	public GameObject Plane;
+
+
+	// Use this for initialization
+	void Start () {
+		GameState.Instance.PrefabsLoaded=true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
